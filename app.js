75e9d852086741465dc7c47492c9f34b7898248a
Prateek Sharma <sharmaprateek.dev@gmail.com>
2022-09-27 15:45:52 +0530

    second update

---
+++ app.js
@@ -0,0 +1,12 @@
+const { log } = require('console');
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is on and listening!");
+})
